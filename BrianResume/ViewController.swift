a83c58396d3b3de8d177a39eb5f8624fd6acfe83
bsberg <bsberg@mtu.edu>
2017-09-28 21:56:35 -0400

    Initial Push
    Small Resume app for iPhone. Interactive Resume

---
+++ BrianResume/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BrianResume
+//
+//  Created by Brian Berg on 2/25/17.
+//  Copyright © 2017 Brian Berg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var NameHeader: UILabel!
+    @IBOutlet weak var EmailHeader: UILabel!
+    @IBOutlet weak var PhoneHeader: UILabel!
+    
+    @IBAction func SkillsButton(_ sender: Any) {
+    }
+    @IBAction func ExperienceButton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
